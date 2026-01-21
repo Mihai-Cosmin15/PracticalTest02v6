125f4c63ddff887d4dd55731fe169f16de9e8d13
mihai <mihaicosmin2003@gmail.com>
2026-01-21 09:13:50 +0200

    Server client

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest02v6/model/Information.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02v6/model/Information.kt
@@ -1,6 +1,9 @@
 package ro.pub.cs.systems.eim.practicaltest02v6.model
 
+import java.time.Instant
+import java.time.LocalDateTime
+
 data class Information (
     val value: String,
-    val updated: String
+    val updated: Instant
 )
--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest02v6/network/CommunicationThread.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02v6/network/CommunicationThread.kt
@@ -1,6 +1,8 @@
 package ro.pub.cs.systems.eim.practicaltest02v6.network
 
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import okhttp3.FormBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -13,13 +15,17 @@ import ro.pub.cs.systems.eim.practicaltest02v6.general.Utilities.getWriter
 import ro.pub.cs.systems.eim.practicaltest02v6.model.Information
 import java.io.IOException
 import java.net.Socket
+import java.time.Instant
+import java.time.LocalDateTime
 import kotlin.text.get
+import kotlin.time.Duration
 
 class CommunicationThread(
     private val serverThread: ServerThread,
     private val socket: Socket
 ) : Thread() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun run() {
         try {
             socket.use { s ->
@@ -45,42 +51,61 @@ class CommunicationThread(
 
                 // DE SCHIMBAT
                 val cached = serverThread.data[coin]
-                val information = cached ?: run {
-                    Log.i(Constants.TAG, "[COMMUNICATION THREAD] Getting the information from the webservice...")
-
-                    val httpClient = OkHttpClient()
-                    // DE SCHIMBAT
-                    val url =
-                        "${Constants.WEB_SERVICE_ADDRESS}$coin"
-
-                    // GET
-                    val request = Request.Builder().url(url).build()
-
-                    val httpResponse = httpClient.newCall(request).execute().use { response ->
-                        if (!response.isSuccessful || response.body == null) {
-                            // error
-                            return;
-                        }
-                        response.body!!.string()
+                val information: Information
+                var found = false
+                if (cached != null) {
+                    if (Instant.now().toEpochMilli() - cached.updated.toEpochMilli() < 10000) {
+                        found = true
                     }
-                    val content = JSONObject(httpResponse)
+                }
+                if (found) {
+                    information = cached!!
+                    Log.d(Constants.TAG, "FOUND IN CACHE")
+                } else {
+                    information = run {
+                        Log.i(
+                            Constants.TAG,
+                            "[COMMUNICATION THREAD] Getting the information from the webservice..."
+                        )
 
-                    // POST
+                        val httpClient = OkHttpClient()
+                        // DE SCHIMBAT
+                        val url =
+                            "${Constants.WEB_SERVICE_ADDRESS}$coin"
+
+                        // GET
+                        val request = Request.Builder().url(url).build()
+
+                        val httpResponse =
+                            httpClient.newCall(request).execute().use { response ->
+                                if (!response.isSuccessful || response.body == null) {
+                                    // error
+                                    return;
+                                }
+                                response.body!!.string()
+                            }
+                        val content = JSONObject(httpResponse)
+
+                        // POST
 //                    val formBody = FormBody.Builder()
 //                        .add("attr1", "value1")
 //                        .build()
 //                    val request = Request.Builder().url("").post(formBody).build()
 
-                    // DE SCHIMBAT POSIBIL
-                    val value = content.getJSONObject("DATA").getJSONObject("BTC-${coin}").getJSONObject("VALUE")
+                        // DE SCHIMBAT POSIBIL
+                        val value = content.getJSONObject("Data").getJSONObject("BTC-${coin}")
+                            .getDouble("VALUE")
 
-                    val info = Information(value = value.toString(), updated = "acum")
+                        val info =
+                            Information(value = value.toString(), updated = Instant.now())
 
-                    // DE SCHIMBAT
-                    serverThread.setData(value.toString(), info)
+                        // DE SCHIMBAT
+                        serverThread.setData(coin.toString(), info)
 
-                    // Return
-                    info
+                        // Return
+                        info
+                    }
+                    Log.d(Constants.TAG, "FROM API CALL")
                 }
 
                 val result = information.value.toString()
